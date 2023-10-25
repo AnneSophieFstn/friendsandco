92a074108945234632ad556c9edace7f14b4407a
Anne-Sophie <annesophie.faustino@gmail.com>
2023-10-25 06:56:46 +0400

    get demande, show friend, add friend

--- server/controller/demande.controller.js
+++ server/controller/demande.controller.js
@@ -1,38 +1,56 @@
+import { Op } from "sequelize";
 import DemandeModel from "../model/demande.model.js";
 import UserModel from "../model/user.model.js";
 
 async function getAmis(req, res) {
   // AVOIR LES DEMANDES D'AMIS QUE LES PERSONNES NOUS A ENVOYÉS
   try {
+    const userId = req.params.id; // ID de l'utilisateur connecté
+
+    // Utilisez Sequelize pour effectuer une jointure entre les tables "Users" et "Demande d'amis"
     const amis = await DemandeModel.findAll({
-      where: { statut: "Amis", id_receveur_user: 1 },
+      where: {
+        statut: "Amis",
+        [Op.or]: [
+          { id_destinataire_user: userId },
+          { id_receveur_user: userId },
+        ],
+      },
+      include: [
+        {
+          model: UserModel,
+          as: "Receveur", // Utilisez l'alias correct ici
+          attributes: ["id", "name", "email"], // Inclure les attributs que vous souhaitez
+        },
+        {
+          model: UserModel,
+          as: "Destinataire", // Utilisez l'alias correct ici
+          attributes: ["id", "name", "email"], // Inclure les attributs que vous souhaitez
+        },
+      ],
     });
 
-    const amisWithName = await Promise.all(
-      amis.map(async (ami) => {
-        const user = await UserModel.findByPk(ami.id_receveur_user);
-        return {
-          ...ami.toJSON(),
-          user,
-        };
-      })
-    ); //test
+    //console.log("amis: ", amis);
 
-    res.status(200).json(amisWithName);
+    res.status(200).json(amis);
   } catch (error) {
     console.log(error);
   }
 }
 async function getDemandeAmisRecu(req, res) {
   // AVOIR LES DEMANDES D'AMIS QUE LES PERSONNES NOUS A ENVOYÉS
+
+  const userId = req.params.id;
+
   try {
+    //console.log("getDemandeAmisRecu: ", req);
     const demandes = await DemandeModel.findAll({
-      where: { id_receveur_user: 1, statut: "En attente" },
+      where: { id_receveur_user: userId, statut: "Attente" },
     });
 
     const demandesWithName = await Promise.all(
       demandes.map(async (demandes) => {
-        const user = await UserModel.findByPk(demandes.id_receveur_user);
+        const user = await UserModel.findByPk(demandes.id_destinataire_user);
         return {
           ...demandes.toJSON(),
           user,
@@ -47,9 +65,13 @@ async function getDemandeAmisRecu(req, res) {
 }
 
 async function getDemandeAmisEnvoyer(req, res) {
+  console.log("getDemandeAmisEnvoyer: ", req.params.id);
+
+  const userId = req.params.id;
+
   try {
     const demandes = await DemandeModel.findAll({
-      where: { id_destinataire_user: 1 },
+      where: { id_destinataire_user: userId, statut: "Attente" },
     });
 
     const demandesWithName = await Promise.all(
@@ -77,7 +99,7 @@ async function createDemande(req, res) {
     const createDemande = await DemandeModel.create({
       id_destinataire_user: req.body.id_destinataire_user,
       id_receveur_user: req.body.id_receveur_user,
-      statut: "En Attente",
+      statut: "Attente",
     });
 
     res.status(200).json({ message: "Demande d'amitié créée avec succès" });
@@ -86,4 +108,49 @@ async function createDemande(req, res) {
   }
 }
 
-export { getDemandeAmisRecu, createDemande, getDemandeAmisEnvoyer, getAmis };
+async function accepterDemande(req, res) {
+  try {
+    /* console.log(req.params);
+    console.log(req.body);
+    console.log(req.query.id); */
+    const accepterDemande = await DemandeModel.update(
+      {
+        id_destinataire_user: req.body.id_destinataire_user,
+        id_receveur_user: req.body.id_receveur_user,
+        statut: req.body.statut,
+      },
+      { where: { id: req.query.id } }
+    );
+    res.status(200).json({ message: "Demande d'amitié accepter avec succès" });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function refuserDemande(req, res) {
+  try {
+    /* console.log(req.params);
+    console.log(req.body);
+    console.log(req.query.id); */
+    const refuserDemande = await DemandeModel.update(
+      {
+        id_destinataire_user: req.body.id_destinataire_user,
+        id_receveur_user: req.body.id_receveur_user,
+        statut: req.body.statut,
+      },
+      { where: { id: req.query.id } }
+    );
+    res.status(200).json({ message: "Demande d'amitié refuser avec succès" });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export {
+  getDemandeAmisRecu,
+  createDemande,
+  getDemandeAmisEnvoyer,
+  getAmis,
+  accepterDemande,
+  refuserDemande,
+};
--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,32 +1,74 @@
-import UserModel from "../model/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { Op } from "sequelize";
+import User from "../model/user.model.js";
+import Demande from "../model/demande.model.js";
 
-async function getAllUser(req, res) {
+async function getUserNotFriend(req, res) {
   try {
-    const users = await UserModel.findAll();
-    return res.status(200).json(users);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur: ",
-      error,
+    /*
+     * 1) Liste les demandes qui comprend l'utilisateur connecter
+     * 2) On parcours la liste de l'étape 1 est on retourne les id des utilisateurs qui on recu ou envoye une demande
+     * 3) On parcours la liste de tout les utilisateurs et on exclus l'id de la personne connecter ainsi que les id de l'étape 2.
+     */
+
+    const userId = req.params.id; // Remplacez 1 par l'ID de l'utilisateur connecté
+
+    // 1) Liste les demandes qui comprend l'utilisateur connecter
+
+    const userDemandeCreer = await Demande.findAll({
+      where: {
+        [Op.or]: [
+          {
+            id_destinataire_user: userId,
+          },
+          {
+            id_receveur_user: userId,
+          },
+        ],
+      },
     });
+
+    // 2) On parcours la liste de l'étape 1 est on retourne les id des utilisateurs qui on recu ou envoye une demande
+
+    const usersInDemandes = userDemandeCreer.map((demande) => {
+      return demande.id_destinataire_user == userId
+        ? demande.id_receveur_user
+        : demande.id_destinataire_user;
+    });
+
+    // 3) On parcours la liste de tout les utilisateurs et on exclus l'id de la personne connecter ainsi que les id de l'étape 2.
+    const utilisateursNonAmis = await User.findAll({
+      where: {
+        id: {
+          [Op.ne]: userId, // Exclut l'utilisateur actuel
+          [Op.notIn]: usersInDemandes, // Exclut les utilisateurs impliqués dans des demandes
+        },
+      },
+    });
+
+    return res.status(200).json(utilisateursNonAmis);
+  } catch (error) {
+    console.log(error);
   }
 }
 
 async function createUser(req, res) {
+  console.log("createUser ok");
   try {
     const saltRounds = 10;
     //
-    if (req.query.name || req.query.email || req.query.password) {
-      const createUser = await UserModel.create({
-        name: req.query.name,
-        email: req.query.email,
-        password: bcrypt.hashSync(req.query.password, saltRounds),
+    if (req.body.name || req.body.email || req.body.password) {
+      const createUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, saltRounds),
       });
     }
 
-    return res.status(200).json({ message: "Utilisateur ajouté avec succès" });
+    return res
+      .status(200)
+      .json({ success: true, message: "Utilisateur ajouté avec succès" });
   } catch (error) {
     console.log(error);
     return res.status(500).json(error);
@@ -35,15 +77,15 @@ async function createUser(req, res) {
 
 async function signIn(req, res) {
   try {
-    const { email, password } = req.query;
+    const { email, password } = req.body;
 
-    if (!req.query.email || !req.query.password) {
+    if (!req.body.email || !req.body.password) {
       return res
         .status(400)
         .json({ message: "Merci de renseigner l'ensemble des informations!" });
     }
 
-    const user = await UserModel.findOne({ where: { email: req.query.email } });
+    const user = await User.findOne({ where: { email: req.body.email } });
 
     if (user && (await bcrypt.compare(password, user.password))) {
       console.log("connexion réussi", user);
@@ -62,6 +104,7 @@ async function signIn(req, res) {
         success: true,
         message: "Vous êtes connecté(e)",
         token,
+        user,
       });
     }
     res.status(400).json("Identifiants invalides");
@@ -73,4 +116,4 @@ async function signIn(req, res) {
   }
 }
 
-export { getAllUser, createUser, signIn };
+export { getUserNotFriend, createUser, signIn };
