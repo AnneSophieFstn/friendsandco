a69413ac0d6d085a43e12421bb8afab9a37549b4
Anne-Sophie <annesophie.faustino@gmail.com>
2023-10-25 06:57:56 +0400

    relation with model user

--- server/model/demande.model.js
+++ server/model/demande.model.js
@@ -1,5 +1,6 @@
 import { Sequelize, DataTypes } from "sequelize";
 import sequelize from "../database/database.js";
+import User from "./user.model.js";
 
 const Demande = sequelize.define("Demande", {
   id: {
@@ -9,18 +10,32 @@ const Demande = sequelize.define("Demande", {
   },
   id_destinataire_user: {
     type: DataTypes.INTEGER,
-    references: "User", // <<< Note, its table's name, not object name
-    referencesKey: "id", // <<< Note, its a column name
+    references: {
+      model: "User", // Nom de la table du modèle "User"
+      key: "id", // Nom de la colonne d'ID dans le modèle "User"
+    },
   },
   id_receveur_user: {
     type: DataTypes.INTEGER,
-    references: "User", // <<< Note, its table's name, not object name
-    referencesKey: "id", // <<< Note, its a column name
+    references: {
+      model: "User", // Nom de la table du modèle "User"
+      key: "id", // Nom de la colonne d'ID dans le modèle "User"
+    },
   },
   statut: {
     type: DataTypes.STRING,
-    defaultValue: "En attente",
+    defaultValue: "Attente",
   },
+});
+
+Demande.belongsTo(User, {
+  foreignKey: "id_receveur_user",
+  as: "Receveur",
+});
+
+Demande.belongsTo(User, {
+  foreignKey: "id_destinataire_user",
+  as: "Destinataire",
 });
 
 export default Demande;
