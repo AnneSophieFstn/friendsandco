2efb64f980e019fb43f73f34d5ed5adb0c898ecf
Anne-Sophie <annesophie.faustino@gmail.com>
2023-10-25 06:57:14 +0400

    modif routes

--- server/routes/demande.routes.js
+++ server/routes/demande.routes.js
@@ -3,15 +3,19 @@ import express from "express";
 import {
   getDemandeAmisRecu,
   createDemande,
+  accepterDemande,
+  refuserDemande,
   getDemandeAmisEnvoyer,
   getAmis,
 } from "../controller/demande.controller.js";
 
 const DemandeRoutes = express.Router();
 //
-DemandeRoutes.get("/demandes-recu", getDemandeAmisRecu);
+DemandeRoutes.get("/demandes-recu/:id", getDemandeAmisRecu);
 DemandeRoutes.post("/demandes", createDemande);
-DemandeRoutes.get("/demandes-envoye", getDemandeAmisEnvoyer);
-DemandeRoutes.get("/amis", getAmis);
+DemandeRoutes.put("/accepter", accepterDemande);
+DemandeRoutes.put("/refuser", refuserDemande);
+DemandeRoutes.get("/demandes-envoye/:id", getDemandeAmisEnvoyer);
+DemandeRoutes.get("/amis/:id", getAmis);
 
 export default DemandeRoutes;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,15 +1,15 @@
 import express from "express";
 
 import {
-  getAllUser,
+  getUserNotFriend,
   createUser,
   signIn,
 } from "../controller/user.controller.js";
 
 const UserRoutes = express.Router();
 
-UserRoutes.get("/users", getAllUser);
-UserRoutes.post("/users", createUser);
+UserRoutes.get("/users-no-friend/:id", getUserNotFriend);
+UserRoutes.post("/inscription", createUser);
 UserRoutes.post("/connexion", signIn);
 
 export default UserRoutes;
